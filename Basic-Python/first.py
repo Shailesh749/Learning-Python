07d469764db4f9c9bdc41402760461b546aea862
Shailesh Kumar <shailesh7492966727@gmial.com>
2025-08-06 21:43:18 +0530

    Complete first.py

--- Basic-Python/first.py
+++ Basic-Python/first.py
@@ -1,2 +1,14 @@
 a = "Hello world"
 print(a)
+print("Hello World", "Welcome to Learning Basic Python in", 16, "Hours" )
+
+rows =5
+for i in range (0, rows):
+   for j in range (0, i+1):
+       print("*", end=" ")
+
+       # end=" "  
+   print("\r") #moves the cursor to the beginning of the current line without advancing to the next line.  
+
+   # print(20+50)
+   # print(10>5)
