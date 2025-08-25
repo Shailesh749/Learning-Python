5a2245eee6290e0fb9f77e74cbf38b94229bf77e
Shailesh Kumar <shailesh7492966727@gmial.com>
2025-08-25 22:11:33 +0530

    Learning user can take input in python how

---
+++ Basic-Python/userInputGet.py
@@ -0,0 +1,25 @@
+# a =input("Enter the Value1:- ")
+# b =input("Enter the Value2:- ")
+# print(a+b) # It's give a string that's why not add
+
+
+
+# Type Casting
+# using type casting we can convert into int or float
+# a = int(input("Enter the Value1:- "))
+# b = int(input("Enter the Value2:- "))
+# print(a+b)
+
+
+
+# a=float(input("Enter the Value1:-"))
+# b=float(input("Enter the Value2:-"))
+# print(a+b)
+
+
+
+
+# eval can take any value ex- int , float, Binary  and convert excute
+a=eval(input("Enter the Valaue1:-"))
+b=eval(input("Enter the Valaue2:-"))
+print(a+b)
